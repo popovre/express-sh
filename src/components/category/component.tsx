d76a02339f042f5a0cafc64a54a8bef083a434a8
Roman <popov.roman.e@gmail.com>
2025-03-18 19:31:06 +0500

    fixed category keys

--- src/components/category/component.tsx
+++ src/components/category/component.tsx
@@ -11,12 +11,12 @@ const CategoryMemoized = ({ category }: CategoryProps) => {
   return (
     <div className={styles.root}>
       <h1 className={styles.name}>name: {category.name}</h1>
-      <div>id: {category.id}</div>
-      <div>slug: {category.slug}</div>
-      <div className={styles.children}>
-        <p>children:</p>
-        {category.children && <RouteLinks categories={category.children} />}
-      </div>
+      {category.children && (
+        <div className={styles.children}>
+          <p>Childrens:</p>
+          <RouteLinks categories={category.children} />
+        </div>
+      )}
     </div>
   )
 }
