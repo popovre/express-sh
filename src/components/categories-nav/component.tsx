3d3cd5dc1f04cf4d7235653a6b08f59b09e7f9ef
Roman <popov.roman.e@gmail.com>
2025-03-18 19:49:42 +0500

    fix styles

--- src/components/categories-nav/component.tsx
+++ src/components/categories-nav/component.tsx
@@ -1,18 +1,28 @@
 import { useSelector } from "react-redux"
 import { getCategoriesEntity } from "../../redux/categories"
 import type { RootState } from "@reduxjs/toolkit/query"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import type { CategoryType } from "../../types"
 import styles from "./styles.module.scss"
+import clsx from "clsx"
 
 const CategoriesNav = () => {
   const data = useSelector((state: RootState) => getCategoriesEntity(state))
+  const location = useLocation()
   return (
     <div className={styles.root}>
       <h3 className={styles.title}>Категории:</h3>
       <div>
         {data.map((category: CategoryType) => (
-          <Link key={"key" + category.slug} to={`./${category.slug}`}>
+          <Link
+            className={clsx({
+              [styles.active]: location.pathname
+                .split("/")
+                .includes(category.slug),
+            })}
+            key={"key" + category.slug}
+            to={`./${category.slug}`}
+          >
             {category.name}
           </Link>
         ))}
